0219c492c1665d9caa0190dde0792dcec311fc03
Sandra Igwegbu <99925559+sandraigwegbu@users.noreply.github.com>
2022-07-16 20:47:06 +0100

    Deleted console outputs
    Deleted console outputs that were remnant from previous console-based iteration of the quiz game

--- main.py
+++ main.py
@@ -9,13 +9,3 @@ for question in question_data:
     question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
-
-
-quiz = QuizBrain(question_bank)
-quiz_ui = QuizInterface(quiz)
-
-# while quiz.still_has_questions():
-#     quiz.next_question()
-
-print("You've completed the quiz")
-print(f"Your final score was: {quiz.score}/{quiz.question_number}")
