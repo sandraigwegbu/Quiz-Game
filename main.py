dfcc30bfcda68ebbead5d39b34e4b3bdc4f47477
Sandra Igwegbu <99925559+sandraigwegbu@users.noreply.github.com>
2022-07-16 20:53:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,8 @@ for question in question_data:
     question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
+
+
+quiz = QuizBrain(question_bank)
+quiz_ui = QuizInterface(quiz)
+
