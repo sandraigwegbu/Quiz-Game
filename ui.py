e15ab6a2d6b6449b3492a99d3b8fd04d2ef66df8
Sandra Igwegbu <99925559+sandraigwegbu@users.noreply.github.com>
2022-07-16 20:42:07 +0100

    GUI App implementation, Trivia Database API
    Program updated to create a more user-friendly experience through the implementation of a tkinter GUI. Previously, hard-coded dictionary of trivia questions replaced by randomly-generated list of true or false questions from Open Trivia Database API.

---
+++ ui.py
@@ -0,0 +1,62 @@
+from tkinter import *
+from quiz_brain import QuizBrain
+
+THEME_COLOR = "#375362"
+FONT = ("Arial", 14, "italic")
+
+
+class QuizInterface:
+	def __init__(self, quiz_brain: QuizBrain):
+		self.quiz = quiz_brain
+		self.window = Tk()
+		self.window.title("Quizzler")
+		self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+		# Label
+		self.score_label = Label(text=f"Score: 0", bg=THEME_COLOR, fg="white")
+		self.score_label.grid(column=1, row=0)
+
+		# Canvas
+		self.canvas = Canvas(height=250, width=300, bg="white", highlightthickness=0)
+		self.question_text = self.canvas.create_text(150, 125, width=275, text="", font=FONT, fill=THEME_COLOR)
+		self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
+
+		# Buttons
+		true_png = PhotoImage(file="./images/true.png")
+		self.true_button = Button(image=true_png, highlightthickness=0, command=self.true_pressed)
+		self.true_button.grid(column=0, row=2)
+
+		false_png = PhotoImage(file="./images/false.png")
+		self.false_button = Button(image=false_png, highlightthickness=0, command=self.false_pressed)
+		self.false_button.grid(column=1, row=2)
+
+		self.get_next_question()
+
+		self.window.mainloop()
+
+	def get_next_question(self):
+		self.canvas.config(bg="white")
+		if self.quiz.still_has_questions():
+			q_text = self.quiz.next_question()
+			self.canvas.itemconfig(self.question_text, text=q_text)
+			self.canvas.config(bg="white")
+			self.score_label.config(text=f"Score: {self.quiz.score}")
+		else:
+			self.canvas.itemconfig(self.question_text, text="You've reached the end of the quiz!")
+			self.true_button.config(state="disabled")
+			self.false_button.config(state="disabled")
+
+	def true_pressed(self):
+		is_right = self.quiz.check_answer(user_answer="True")
+		self.give_feedback(is_right)
+
+	def false_pressed(self):
+		is_right = self.quiz.check_answer(user_answer="False")
+		self.give_feedback(is_right)
+
+	def give_feedback(self, is_right):
+		if is_right:
+			self.canvas.config(bg="green")
+		elif not is_right:
+			self.canvas.config(bg="red")
+		self.window.after(1000, self.get_next_question)
